86a988580a128c4c2de55f608354349ce5755fde
Jehan <jehan.tremback@gmail.com>
2018-02-12 13:50:07 -0800

    getr basic http reqs set up in bounty dash

--- bounty-hunter-dash/src/App.js
+++ bounty-hunter-dash/src/App.js
@@ -1,8 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
+
+const statServer = "http://127.0.0.1:8080/mock.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { nodes: [] };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.updateNodes(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  async updateNodes() {
+    const rand = Math.random();
+    console.log("req", rand);
+    const nodes = await (await fetch(statServer)).json();
+    console.log("res", rand, nodes);
+    this.setState({ nodes });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +37,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="danger">
+          <h2>It is {JSON.stringify(this.state.nodes)}.</h2>
+        </Button>
       </div>
     );
   }
