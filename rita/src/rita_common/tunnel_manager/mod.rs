8134cb1e15fc9c01dc4dbb0c2bbca1a25c8cf6eb
Michał Papierski <michal@papierski.net>
2018-08-06 18:12:08 +0200

    Added a simple test for tunnel manager.

--- rita/src/rita_common/tunnel_manager/mod.rs
+++ rita/src/rita_common/tunnel_manager/mod.rs
@@ -418,3 +418,9 @@ impl TunnelManager {
         }
     }
 }
+
+#[test]
+pub fn test_tunnel_manager() {
+    let mut tunnel_manager = TunnelManager::new();
+    assert_eq!(tunnel_manager.get_port(), 65499);
+}
